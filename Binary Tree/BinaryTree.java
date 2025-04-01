46da718c22963a0bf0bbf401d65269e18ac17ed0
manukarnikas <manukarnikasud@gmail.com>
2025-04-01 22:50:37 +0530

    added delete

--- Binary Tree/BinaryTree.java
+++ Binary Tree/BinaryTree.java
@@ -28,6 +28,54 @@ class Tree{
         return root;
     }
 
+    int search(Node root,int val){
+        if(root == null){
+            return -1;
+        }else if(root.val == val){
+            return 0;
+        }else if(val<root.val){
+            return search(root.left,val);
+        }else{
+            return search(root.right,val);
+        }
+    }
+
+    Node findMinInRightSubtree(Node root){
+        while(root.left!=null){
+            root = root.left;
+        }
+        return root;
+    }
+
+    Node delete(Node root,int val){
+        if(root == null){
+            return root;
+        }else if(val<root.val){
+            root.left =  delete(root.left,val);
+        }else if(val>root.val){
+            root.right = delete(root.right,val);
+        }else{
+            // no child
+            if(root.left == null && root.right==null){
+                return null;
+            }
+
+            //one child
+            if(root.left==null && root.right!=null){
+                return root.right;
+            }
+            if(root.left!=null && root.right==null){
+                return root.left;
+            }
+
+            //two child
+            Node selectedNode = findMinInRightSubtree(root.right);   // or can use find max in left subtree
+            root.val = selectedNode.val;
+            root.right = delete(root.right,selectedNode.val);
+        }
+        return root;
+    }
+
     void preorderTraversal(Node root){
         if(root == null){
             return;
@@ -72,5 +120,15 @@ class BinaryTree {
         tree.inorderTraversal(tree.root);
         System.out.println("\nPostorder Traversal:");
         tree.postorderTraversal(tree.root);
+        int search_value = 6;
+        int result = tree.search(tree.root, search_value);
+        if(result < 0){
+            System.out.println("\n Value not found: "+search_value);
+        }else{
+            System.out.println("\nValue found: "+search_value);
+        }
+        tree.root = tree.delete(tree.root,5);
+        System.out.println("\nInorder Traversal:");
+        tree.inorderTraversal(tree.root);
     }
 }
